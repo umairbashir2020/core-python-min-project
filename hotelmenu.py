5832ace9f87b1ab6d7f072d2891063cc28938dac
umairbashir2020 <umairbashir2020@gmail.com>
2025-09-27 23:59:31 -0700

    hotel restaurant management

---
+++ hotelmenu.py
@@ -0,0 +1,15 @@
+# Define the menu of restaurant
+
+menu_list = {
+    "Burger": 150,
+    "Pizza": 700,
+    "French Fries": 100,
+    "Sandwich": 70,
+    "Milkshake": 100,
+    "Chicken Nuggets": 120
+}
+
+# Greet
+print('Welcome to PYTHON RESTAURANT')
+
+print(" Burger: Rs150\n Pizza: Rs700\n French Fries: Rs100\n Sandwich: Rs70\n MilkShake: Rs100\n Chicken Nuggests: Rs120")
